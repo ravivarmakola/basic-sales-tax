fe1c6474d9c5b1a53d98e785045388143bda9f23
ravivarmakola <ravi.iiet@gmail.com>
2021-09-21 10:25:30 +0200

    Added code changes to read file in main

--- src/main/java/com/itemis/bst/BasicSalesTaxMain.java
+++ src/main/java/com/itemis/bst/BasicSalesTaxMain.java
@@ -1,7 +1,24 @@
 package com.itemis.bst;
 
+import com.itemis.bst.io.IOUtil;
+import com.itemis.bst.model.Item;
+import lombok.extern.java.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+
+@Log
 public class BasicSalesTaxMain {
     public static void main(String[] args) {
-
+        String filePath = System.getProperty("input-path");
+        try {
+            List<Item> itemList = IOUtil.readFile(filePath);
+            log.info(itemList.toString());
+            // TODO Invoke Tax Processing Engine to process the items
+            // TODO Write the output to file
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Either the path ({0}) is wrong or the file doesn't exists", filePath);
+        }
     }
 }
